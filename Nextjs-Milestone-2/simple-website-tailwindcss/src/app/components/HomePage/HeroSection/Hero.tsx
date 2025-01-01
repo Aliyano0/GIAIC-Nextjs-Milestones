3409ac1a69dd6b9152f8cab5b72871866a0cf5c8
Aliyano0 <aliyanaqeel97@gmail.com>
2025-01-01 06:26:55 +0500

    Fixed Hero Section

--- Nextjs-Milestone-2/simple-website-tailwindcss/src/app/components/HomePage/HeroSection/Hero.tsx
+++ Nextjs-Milestone-2/simple-website-tailwindcss/src/app/components/HomePage/HeroSection/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="section ml-[-10px] w-full mt-[60px] lg:mt-0 mb-20 xl:mt-20 xl:mb-[120px] overflow-x-hidden ">
       <div className="heroContainer flex-col justify-start lg:justify-normal h-auto lg:flex-row w-full xl:w-[1200px] lg:h-[500px] mx-auto flex items-center relative px-6 ">
       <div className="herotextcontainer w-full xs:w-[95%] sm:w-[80%] lg822:w-[60%]  lg:w-[42%] xl:w-[45%] mt-6">
-      <div className="HeroText">
+      <div className="HeroText w-full flex justify-center items-center flex-col text-center lg:text-left lg:block lg:w-auto">
         <h5 className="companyName text-sm leading-6 text-[#d77474] font-normal tracking-[2px] mb-2 ">POMPEO POTTERY</h5>
         <h2 className="heading text-4xl xs:text-[48px] lg:text-[40px] xl:text-5xl xl:leading-[58px] font-medium mb-2 ">Unique Porcelain <span className="playfair-display text-[68px] leading-[68px] font-normal text-main-red">&</span> Stone Collection</h2>
         <p className="para libre-franklin leading-[26px] w-[80%] lg:w-auto text-sm xs:text-base text-main-gray">Unique & modern pottery made by our master in porcelain & stones</p>
