c86b81e8d4069aa4eeb5d7c24bd14a513077e1c3
Aliyano0 <aliyanaqeel97@gmail.com>
2025-01-08 07:01:39 +0500

    Solved linting problem

--- Nextjs-Milestone-2/simple-website-custom-css/src/app/components/HomePage/PotteryCollection/PotteryCollection.tsx
+++ Nextjs-Milestone-2/simple-website-custom-css/src/app/components/HomePage/PotteryCollection/PotteryCollection.tsx
@@ -1,10 +1,9 @@
-import { Linden_Hill } from "next/font/google";
 import ContainerHeadings from "../../Utils/ContainerHeadings/ContainerHeadings";
 import ProductCard from "../../Utils/ProductCard/ProductCard";
 import RedButton from "../../Utils/RedButton/RedButton";
 import VerticalText from "../../Utils/VerticalText/VerticalText";
 import styles from "./PotteryCollection.module.css";
-import {productData }from "./productData";
+import { productData } from "./productData";
 import Link from "next/link";
 
 const PotteryCollection = () => {
@@ -12,28 +11,29 @@ const PotteryCollection = () => {
     <section className={styles.section}>
       <VerticalText text="Product Collections" />
       <div className={styles.container}>
-        <ContainerHeadings subtitle="Our online store" heading="Pottery Collection"/>
+        <ContainerHeadings
+          subtitle="Our online store"
+          heading="Pottery Collection"
+        />
         <div className={styles.collectionGridContainer}>
-          {
-            productData.map((product) => (
-              <ProductCard 
-                key={product.id}
-                productImage={product.productImage}
-                productName={product.productName}
-                price={product.price}
-                altText={product.altText}
-              />))
-          }
+          {productData.map((product) => (
+            <ProductCard
+              key={product.id}
+              productImage={product.productImage}
+              productName={product.productName}
+              price={product.price}
+              altText={product.altText}
+            />
+          ))}
         </div>
         <div className={styles.btnContainer}>
           <Link href="/shop">
-          <RedButton btnText="View All Products" />
+            <RedButton btnText="View All Products" />
           </Link>
         </div>
       </div>
-      
     </section>
-  )
-}
+  );
+};
 
-export default PotteryCollection
+export default PotteryCollection;
